0bac6ec6e214012e7d62e56a48666480eb0437d7
manhhb237 <hoangmanh2803@gmail.com>
2026-04-22 11:54:04 +0900

    Remove default Soniox key - users enter their own (saved to localStorage)

--- public/config.js
+++ public/config.js
@@ -7,5 +7,3 @@ var FIREBASE_CONFIG = {
     messagingSenderId: "101689607278",
     appId: "1:101689607278:web:83851f950f2e579a7a527f"
 };
-
-var DEFAULT_SONIOX_KEY = "faf2be22dbcb17fa6401cae0a9d207f5494b79b2f8892ff0f40974984c9d1f12";
