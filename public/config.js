9ec5b07792ae055305af0bbcccf25e3e77d54822
manhhb237 <hoangmanh2803@gmail.com>
2026-04-22 11:24:46 +0900

    Update Firebase config with real project credentials (manhrealtimetrans-c7a1c)

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 var FIREBASE_CONFIG = {
-    apiKey: "",
-    authDomain: "manhrealtimetrans.firebaseapp.com",
-    databaseURL: "https://manhrealtimetrans-default-rtdb.firebaseio.com",
-    projectId: "manhrealtimetrans",
-    storageBucket: "manhrealtimetrans.firebasestorage.app",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCQbfDz_xiL5zc8JYC-GuBLgp1vg9ThX4w",
+    authDomain: "manhrealtimetrans-c7a1c.firebaseapp.com",
+    databaseURL: "https://manhrealtimetrans-c7a1c-default-rtdb.firebaseio.com",
+    projectId: "manhrealtimetrans-c7a1c",
+    storageBucket: "manhrealtimetrans-c7a1c.firebasestorage.app",
+    messagingSenderId: "592069137508",
+    appId: "1:592069137508:web:e65f769b824ce151c8bb71"
 };
